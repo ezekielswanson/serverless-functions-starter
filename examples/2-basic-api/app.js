d073accfa3f3172e9bdc3790accb729c203df0ad
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-07-31 21:22:36 -0500

    Updated 2-basic-api function

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -14,18 +14,17 @@ const fetchData = async () => {
         return `
             <article class="product">
                 <img
-                    src="https://dl.airtable.com/.attachments/6ac7f7b55d505057317534722e5a9f03/9183491e/product-3.jpg"
+                    src="${url}"
                     alt="utopia sofa"
                 />
                 <div class="info">
-                    <h5>utopia sofa</h5>
-                    <h5 class="price">$39.95</h5>
+                    <h5>${name}</h5>
+                    <h5 class="price">${price}</h5>
                 </div>
             </article>
         `
       })
-
-      //console.log(data);
+      console.log(data);
       result.innerHTML = `<h2>GOD is good</h2>`
     } catch (error) {
       result.innerHTML = `<h4>Error, try again later</h4>`
