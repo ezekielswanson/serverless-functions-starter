3735892145999c19d4cd0bb47f03bcc5c862ad3a
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-08-02 21:28:59 -0500

    Updated COORS origin.

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -8,7 +8,11 @@ const items = require('../assets/data.js')
 //Domain/.netlify/functions/1-hello
 exports.handler = async (event, context, cb) => {
     return {
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
         statusCode: 200,
         body: JSON.stringify(items),
     }
 }
+
