a1baba8b360f0344d009c96d738dd89621846e12
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-08-07 22:01:29 -0500

    Update Airtable configuration and add serverless function
    - Implement Airtable connection using airtable-node package
    - Configure Airtable with Personal Access Token, base ID, and table name
    - Add serverless function with a simple response

---
+++ functions/3-airtable.js
@@ -0,0 +1,16 @@
+
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: 'patV82muFEGIJENN9.042040bf5e397066ff9e4ad3374190a70337c8f2fb6b51d26062d63ac6a143d9' })
+  .base('appc5JvV86warsg5A')
+  .table('products')
+ 
+
+
+//Domain/.netlify/functions/1-hello
+exports.handler = async (event, context, cb) => {
+    return {
+        statusCode: 200,
+        body: 'I love solving challenging problems to help others :)'
+    }
+}
