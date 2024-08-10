18c77f9fe8aa6e67da0c972e8f045653d2b14a78
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-08-09 20:49:39 -0500

    Update 3-air table function
    Pulled in Product info

--- functions/3-airtable.js
+++ functions/3-airtable.js
@@ -1,33 +1,32 @@
-
 const Airtable = require('airtable-node');
- 
+
 const airtable = new Airtable({ apiKey: 'patV82muFEGIJENN9.042040bf5e397066ff9e4ad3374190a70337c8f2fb6b51d26062d63ac6a143d9' })
   .base('appc5JvV86warsg5A')
   .table('products')
- 
 
-
-//Domain/.netlify/functions/1-hello
-exports.handler = async (event, context, cb) => {
-
+// Domain/.netlify/functions/1-hello
+exports.handler = async (event, context) => {
   try {
-    const {records} = await airtable.list();
-    //console.log(data);
+    const { records } = await airtable.list();
     console.log(records);
 
-    records.map(record => {
-      
+    const products = records.map(product => {
+      const { id } = product;
+      const { Name, image, price } = product.fields;
+      // Accessing first img in the loop
+      const url = image[0].url;
+      return { id, name: Name, url, price };
+    });
 
-
-    })
-
-    
-
-
-
-  } catch (eroror) {}
     return {
-        statusCode: 200,
-        body: 'I love solving challenging problems to help others :)'
-    }
-}
+      statusCode: 200,
+      body: JSON.stringify(products)
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Data was not returned.' })
+    };
+  }
+};
