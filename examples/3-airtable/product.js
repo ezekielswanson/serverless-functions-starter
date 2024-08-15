5a973d645e5aa85240f32811b6fa641e3d25ebbc
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-08-14 21:49:06 -0500

    Create fetchProdcut function
    Filtering based on id in the url slug

---
+++ examples/3-airtable/product.js
@@ -0,0 +1,34 @@
+
+const result = document.querySelector('.result');
+
+const fetchProduct = async () => {
+
+
+    try {
+        const id = window.location.search;
+        //console.log(id);
+
+        const {data:{fields}} = await axios.get(`/api/3-product${id}`)
+        const {name, desc, price, image} = fields;
+        result.innerHTML = `
+            <h1 class="title">${name}</h1>
+            <article class="product">
+                <img class="product-img"
+                src="${image[0].url}"
+                alt="${$name}"
+                />
+                <div class="product-info">
+                <h5 class="title">${name}</h5>
+                <h5 class="price">$${name}</h5>
+                <p class="desc">${desc}</p>
+                </div>
+            </article>
+        `
+        //console.log(data);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+fetchProduct();
