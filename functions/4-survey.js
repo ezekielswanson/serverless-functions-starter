84bfe9b28c056391ab95a52bcfbaab3ca91c307b
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-08-17 13:44:22 -0500

    Update 4-survey.js

--- functions/4-survey.js
+++ functions/4-survey.js
@@ -1,8 +1,60 @@
 
-//Domain/.netlify/functions/1-hello
+//Installing Node wrapper for Airtable API
+const Airtable = require('airtable-node');
+require('dotenv').config();
+
+//Accessing Airtable via API Key, Base ID, & Table Name
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+    .base('appc5JvV86warsg5A')
+    .table('survey');
+
+
+//Domain/.netlify/functions/4-survey.js
 exports.handler = async (event, context, cb) => {
-    return {
-        statusCode: 200,
-        body: 'I love solving challenging problems to help others :)'
+    try {
+
+        /*
+        const response = await airtable.list();
+        console.log(response.records);
+        */
+        
+        const {records} = await airtable.list();
+        console.log(records);
+
+        const survey = records.map(item => {
+            const {id} = item;
+            const {room,votes} = item.fields
+            return {id, room, votes}
+        })
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(survey),
+        }
+
+        
+
+
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: 'Server erro please check logs'
+        }
+
+
     }
+
+    
+  
+    
 }
+
+
+
+
+
+
+
+
+
+
