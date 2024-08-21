067aa9ab101deaa7869ee561209efb62f55773ca
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-08-20 21:37:07 -0500

    Update fetchVotes function

--- examples/4-survey/app.js
+++ examples/4-survey/app.js
@@ -8,6 +8,8 @@ const result = document.querySelector('.result')
 1. fetch data from sf
 
 
+dsiplaying here
+ <ul class="result"></ul>
 
 
 */
@@ -15,15 +17,44 @@ const result = document.querySelector('.result')
 
 const fetchVotes = async () => {
     try {
+        //fetch data from serverless
+
+        /*
+        const {data} = await axios.get('/api/4-survey');
+        const res = votes(vote => {
+        */
+
         const res = await axios.get('/api/4-survey');
 
+        //store data in var
         const votes = res.data;
-        console.log(votes);
+
+        //do something w/ the data
+        const votesHTML = votes(vote => {
+            const {id, room, votes } = vote
+
+            return `
+                <li>
+                    <div class="key">
+                        ${room.toUpperCase().substring(0,2)}
+                    </div>
+                    <div>
+                        <h4>
+                            ${room}
+                        </h4>
+                        <p class="vote-${id}" data-votes="${votes}">${votes}</p>
+                    </di>
+                </li>
+            `
+        }).join('');
+        
+        result.innerHTML = votesHTML;
+
 
 
 
     } catch (error) {
-
+        result.innerHTML = `<h4>Error fetching data</h4>`
     }
 
 }
@@ -31,3 +62,4 @@ const fetchVotes = async () => {
 
 
 fetchVotes();
+
