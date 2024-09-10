a566bde7788185f9c4af66c009cdaa803ea29602
ezekielswanson <75798690+ezekielswanson@users.noreply.github.com>
2024-09-09 20:57:07 -0500

    Create 5-weather.js

--- examples/5-weather/app.js
+++ examples/5-weather/app.js
@@ -5,3 +5,38 @@ const input = document.querySelector('.form-input')
 const alert = document.querySelector('.alert')
 const result = document.querySelector('.result')
 alert.style.display = 'none'
+
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const city = input.value;
+    console.log(city);
+
+    if (city) {
+        getWeatherData(city);
+    }
+
+
+})
+
+
+
+//Access data from the serverless function
+async function getWeatherData(city) {
+    alert.style.display = 'none'
+
+    try {
+        const {data} = await axios.get('/api/5-weather',{city});
+        console.log(data);
+
+
+
+    } catch (error) {
+        console.log(erro.response)
+    }
+}
+
+
+
+getWeatherData();
--- functions/5-weather.js
+++ functions/5-weather.js
@@ -1,6 +1,9 @@
 
 
 exports.handler = async (event, context, cb) => {
+    const method = event.httpMethod;
+    console.log(method);
+    
     return {
         statusCode: 200,
         body: 'I love solving challenging problems to help others :)'
